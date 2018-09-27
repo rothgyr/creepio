167c050366e5cff61074401a96f92f04c19a6493
rothgyr <rothgyr@gmail.com>
2018-09-27 09:39:01 -0600

    LOLZ

---
+++ tmp.php
@@ -0,0 +1,140 @@
+******************************************************************************************
+LIGHT SIDE (15)
+******************************************************************************************
+
+Force Supression - separate yourself from the Force (Luke does it)
+Wall of light - Light Side - used by multiple members of the light side to purge dark side power, or dark side users from their connection to the force
+Force light - LIght Side - Disrupt a dark side user’s connection from the force (calm|dampen emotions)
+Revitalize - Light Side - revitalize an exhausted, wounded, or unconcious person
+Sever Force - Light Side - High level - cut off or block another person from the force
+Conceal other - conceal other people via the Force
+Plant surge - Light Side - control or even cause growth in plants and control the plant
+Cleanse Mind - Light Side - purge others of Force afflictions
+Detoxify poison - self explanatory
+Revelation - Light Side - backlight enemies in hiding, revealing their location
+Malacia - Light Side - cause disequilibrium in target that is nausea inducing and completely incapacitating them or hack them and imbue force intelligence of sorts
+Force blinding - Light Side - flash of light to blind foes temporarily
+Alter damage - Light Side - control how much damage you do, both in restraint, or focus
+Ionize - cause short circuiting in droids
+Force Hybernation - nybernate and appear dead
+Force healing - SE
+Theran force listening - Light Side - listen via the force instead of ears. Can understand unknown languages
+
+******************************************************************************************
+3-4
+******************************************************************************************
+
+Force combust - SE
+
+
+
+******************************************************************************************
+NEUTRAL
+******************************************************************************************
+
+crush organs (Neutral)
+Force Wound - choke, grip, crush, etc.
+Alter environment - cause stroms, earthquakes etc.
+Darkshear - Dark Side - Spear of Midnight black - forge an imperceivable force weapon in the form of a spear
+Memory walk - Dark Side - victims relive (worst) memories. After a brief torture session subjects demonstrate a slight increase in intenligence
+
+******************************************************************************************
+2-3
+******************************************************************************************
+
+Foce hide alignment - SE
+Drain Knowledge - Dark Side - Kylo Ren uses this on Poe and Rey
+Crucitorn - agrivate another’s sensation of pain, or (b) use the force to tear into existing wounds
+
+
+
+
+******************************************************************************************
+DARK SIDE (18)
+******************************************************************************************
+
+Thought bomb - Dark side - RITUAL - like Philosopher’s Stone in FMAB
+Transfer Essence - Dark Side - transfer your consciousness from body to body on death, or even into an inanimate object. Can haunt a tomb and posess!
+Force Alchemy - Dark Dark Side - genetically alter composition alteration (enhance metals) necromancy
+Force storm - Dark Side - open a hyperspace wormhole or portal to destroy up to a planet’s surface
+Dark Transfer - RARE - Dark Side - resurrect one who is “mostly dead” and not “all dead”
+plague - fast acting force toxin that worsens with time and leads to death
+Dark Sight - Dark Side - inflict pain by line of sight
+kill - destroy organs (Dark Side)
+Dark healing - siphen the life of another to heal yourself
+Affliction - eventual debilitation, possibly death
+Kinetite - Dark Side - throw a ball of force lightning or use it to deflect powers
+Sith Poison - make crazzzzzzy poisons
+force insanity
+force horror
+Force destruction - Dark Side - raw force energy that vaporizes anything in the unleashed path
+Force drain life - Dark Side -drain the life out of a victim
+Force Lightning - Dark Side - se
+Force Scream - Dark Side - “NOOOOOOOOOOOOOOOOOOOO” - Darth Vader
+Force Slow - Dark Side - cloud the persons mind and body. Mild version of force plague
+Mechu-Deru - Dark Side - grants understanding of droid internals and allows the user to control
+
+
+
+
+CONTROL
+
+Force ghost - SE
+Center of bing - meditation stance that allows for blocking in nearly all positions
+art of movement - higher form of movement
+breath control - suppress the need to breath
+force enlightenment - maximize the powers effect (x2 or soemthing)
+Force speed - SE
+Tapas - warm despite the cold via the force
+Force active camoflauge - rare, SE
+
+SENSE
+
+Shatterpoint - find physical impurities/breaks, and influecne them to just break
+Force smell - SE
+Force sense - emothion, dangers, etc.
+Precognition - SE
+Force sight - depend on the Force instead of eyes for sight
+Psychometry - sense the history of a given object to find clues, etc.
+Force Empathy - sense another’s feelings (I can feel your anguh)
+Farsight - vague impressions of what’s going on elsewhere via the force
+Telepathy
+Mid-battle Precognition -SE
+
+ALTER
+Animal bond - via the force have an animal companion
+Battle meditation - assist via the force, especially for coordination, or lending focus to another
+Force bellow - amplify your voice
+Force confusion - cause confusion in the target
+Tutaminis - absorb energies, yoda absorbed lightning, vader did this to Han’s blaster bolts
+Force deflect - deflect lasers, etc. to a different direction
+Force flash - disrupt camera feeds
+Force flight- SE
+Force illusion - like doppleganger, but whole vistas could be projected on a victim’s senses
+Shock
+Doppleganger - Luke uses this on Kylo Ren
+Storm
+force bubble - air pocket under water
+Force Jump - SE
+force pull | push - SE
+force persuasion - jedi mind trick, SE
+Force Stun - Kylo Ren uses this on Rey
+stasis - higher level, on multiple targets
+Force Weapon - SE
+Force Whirlwind
+Pyrokinesis - use force friction to generate fire
+Dampen presence - dampen your force presence
+Alter image - disguise your look via the force. Require -1 ongoing for ability casting
+Force Travel - Anyone - teleport, but it corrupts you to the Dark Side as it tears the Force asunder
+
+
+
+
+
+
+
+
+
+
+
+
